0a9c42a16d52e6da3e73fe248b4959542d28ce8e
TwitchViejoSarmi <alesansarive@gmail.com>
2025-03-11 20:45:05 -0500

    MOD: Ajustado debido a dependencia de librerias

--- insertdata.py
+++ insertdata.py
@@ -3,16 +3,14 @@ import json
 from pathlib import Path
 from dotenv import load_dotenv
 import boto3
-import psycopg2
+import pg8000
 
-# Load enviornment.
+# Load environment variables
 load_dotenv()
 
 def get_s3_session():
     """
-        Function that returns the active session on the AWS CLI.
-        Returns:
-            (any): The actual session.
+    Returns an active AWS session.
     """
     session = boto3.Session(
         aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
@@ -20,69 +18,58 @@ def get_s3_session():
         aws_session_token=os.getenv("AWS_SESSION_TOKEN"),
         region_name=os.getenv("AWS_REGION")
     )
-
     return session
 
 def get_sql_session():
     """
-        Function that returns an active cursos on PostgreSQL.
-        Returns:
-            (any): The actual cursos.
+    Returns an active PostgreSQL connection and cursor.
     """
-    conn = psycopg2.connect(
-        dbname=os.getenv("DB_NAME"),
+    conn = pg8000.connect(
+        database=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT")
+        port=int(os.getenv("DB_PORT"))
     )
-
     cur = conn.cursor()
-
     return conn, cur
 
-def insert_data(data:dict, conn, cur):
+def insert_data(data: dict, conn, cur):
     """
-        Function that insert the data in the table datalogs in PostgreSQL.
-        Params:
-            data (dict): The data to insert.
-            conn: The connection on PostgreSQL.
-            cur: The cursor of PostgreSQL.
+    Inserts the data into the `datalogs` table in PostgreSQL.
     """
-    query = "INSERT INTO datalogs (sensor_id, timestamp, temperature, humidity, location, battery_level) VALUES (%s,%s,%s,%s,POINT(%s,%s),%s)"
-    inserts = (data["sensor_id"], data["timestamp"], data["temperature"], data["humidity"], data["location"]["latitude"], data["location"]["longitude"], data["battery_level"])
-    # Execute the query.
+    query = """
+        INSERT INTO datalogs (sensor_id, timestamp, temperature, humidity, location, battery_level)
+        VALUES (%s, %s, %s, %s, POINT(%s, %s), %s) ON CONFLICT (sensor_id, timestamp) DO NOTHING
+    """
+    inserts = (
+        data["sensor_id"], data["timestamp"], data["temperature"],
+        data["humidity"], data["location"]["latitude"],
+        data["location"]["longitude"], data["battery_level"]
+    )
+    
     cur.execute(query, inserts)
-
-    # Commit the changes.
     conn.commit()
     print(f"Inserted {data}")
 
-def get_json_files(bucket_name:str, session):
+def get_json_files(bucket_name: str, session):
     """
-        Gets all the json files in the Bucket S3.
-        Params:
-            bucket_name (str): The name of the bucket S3.
-            session (any): The active session on AWS CLI.
+    Gets all JSON files from an S3 bucket and inserts them into the database.
     """
     s3 = session.client("s3")
-    response = s3.list_objects_v2(Bucket=bucket_name) # Get the files in the bucket.
+    response = s3.list_objects_v2(Bucket=bucket_name)
     conn, cur = get_sql_session()
 
-    # Verify if there are files.
     if "Contents" in response:
-        # Get each file.
         for obj in response["Contents"]:
             file = obj["Key"]
             file_data = s3.get_object(Bucket=bucket_name, Key=file)
             extension = Path(file).suffix.lower()
 
             if extension == ".json":
-                # Get the file content and convert it to dictionary.
                 content = file_data["Body"].read().decode("utf-8")
                 data = json.loads(content)["measurements"]
                 for register in data:
-                    print(register)
                     insert_data(register, conn, cur)
     else:
         print("No files detected...")
